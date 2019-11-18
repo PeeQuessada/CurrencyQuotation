00cf0c816d72ef4d25b0db723050c181425e8b8a
PeeQuessada <pedroquessad@gmail.com>
2019-11-18 16:30:04 -0300

    Bug em agendamento

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,12 @@
+declare module "@salesforce/label/c.BaseCurrency" {
+    var BaseCurrency: string;
+    export default BaseCurrency;
+}
+declare module "@salesforce/label/c.CurrencyUpdate" {
+    var CurrencyUpdate: string;
+    export default CurrencyUpdate;
+}
+declare module "@salesforce/label/c.EndpointAPICurrency" {
+    var EndpointAPICurrency: string;
+    export default EndpointAPICurrency;
+}
